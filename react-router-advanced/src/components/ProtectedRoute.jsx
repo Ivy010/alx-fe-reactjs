0b4dbfd986f79277a91bf99868b357afb15bbb26
Ivy010 <irmmwangi@gmail.com>
2025-03-09 20:01:54 +0300

    feat: Completed set up for useAuth

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-
+import useAuth from './useAuth';
 const ProtectedRoute = ({ isAuthenticated, children }) => {
     return isAuthenticated ? children : <Navigate to="/" />;
 };
