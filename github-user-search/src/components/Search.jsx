1f74b28e2e37157904a25d7b8c260a95a0ca15fe
Ivy010 <irmmwangi@gmail.com>
2025-03-26 23:27:21 +0300

    Updated the search file

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -39,7 +39,7 @@ return (
 
 );
     {loading && <p>Loading...</p>}
-    {error && <p>"Looks like we can't find the user"</p>}
+    {error && <p>"Looks like we cant find the user"</p>}
     {userData && (
      <div>
          <img src={userData.avatar_url} alt={`${userData.login}'s avatar`} width="100" />
