a38dee3001404282e35b49b69f1adbdaf946c227
Ivy010 <irmmwangi@gmail.com>
2025-03-26 20:43:35 +0300

    Created new react app and installed all software needed

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <Router>
+
+    </Router>
+  )
+}
+
+export default App
