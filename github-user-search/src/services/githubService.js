8879092c6af1bb1e971a0f31db48db70655e3e29
Ivy010 <irmmwangi@gmail.com>
2025-03-27 22:11:44 +0300

    Updated the githubservice file

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const fetchUserData = async (username, location, minRepos) => {
     try {
         const query = `q=${username ? `${username}+` : ''}${location ? `location:${location}+` : ''}${minRepos ? `repo:>=${minRepos}` : ''}`;
-        const response = await axios.get('https://api.github.com/users/{username}');
+        const response = await axios.get("https://api.github.com/search/users?q");
         return response.data;
     }   catch (error) {
         throw new Error('Looks like we cant find the user')
